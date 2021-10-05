4da839f7131d4e3b457e347ed13b0723b54a6223
Radek Nowakowski <rad.nowakowski@gmail.com>
2021-10-05 22:52:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,72 +1,73 @@
 let Array = [
-  'over the horizon',
-  'boots on ground',
-  'pomost bałtycko-czarnomorski',
-  'gra o sumie zerowej',
-  'łańcuchy dostaw',
-  'Szojgu',
-  'Mackinder',
-  'Persowie',
-  'ludzie morza',
-  'Armia Nowego Wzoru',
-  'języczek u wagi',
-  'polityka hetmanów',
-  'projekcja siły',
-  'ocean światowy',
-  'eszelon',
-  'lewar',
-  'piwot',
-  'Pax Americana',
-  'pętla decyzyjna',
-  'drabina eskalacyjna',
-  'przesmyk suwalski',
-  'wektor',
-  'król Salomon',
-  'deep penetration',
-  'connectivity',
-  'Heartland',
-  'Anglosasi',
-  'brama smoleńska',
-  'przepływy strategiczne',
-  'doktryna Giedroycia',
-  'wykrok',
-  'pauza geopolityczna',
-  'koncert mocarstw',
-  'matryca',
-  'wieża',
-  'strefa zgniotu',
-  'Państwo Środka',
-  'wschodnia flanka NATO',
-  'equilibirum',
-  'wysunięta kasztelania',
-  'masy lądowe Eurazji',
-  'kolaps',
-  'Rimland',
-  'Szlak Jedwabny',
-  'junior partner',
-  'Clausewitz',
-  'punkt ciężkości',
-  'gra statusowa',
-  'sworzeń',
-  'Lewant',
-  'systemy anty- dostępowe',
-  'ekspirowanie',
-  'wojna hybrydowa',
-  'stronnictwo kontynentalne',
-  'obszar rdzeniowy',
-  'efektor',
-  'długie trwanie',
-  'iunctim',
-  'domena kosmiczna',
-  'agregacja informacji',
-  'Boca Chica',
-  'baronowie kosmosu',
-  'zeloci niepodległości',
-  'neoantlantyści',
-  'mapy mentalne',
-  'dualizm na Łabie',
-  'układ sił',
-  'pułapka Tukidydesa',
+  'over the horizon ',
+  'boots on ground ',
+  'pomost bałtycko-czarnomorski ',
+  'gra o sumie zerowej ',
+  'łańcuchy dostaw ',
+  'Szojgu ',
+  'Mackinder ',
+  'Persowie ',
+  'ludzie morza ',
+  'Armia Nowego Wzoru ',
+  'języczek u wagi ',
+  'polityka hetmanów ',
+  'projekcja siły ',
+  'ocean światowy ',
+  'eszelon ',
+  'lewar ',
+  'piwot ',
+  'Pax Americana ',
+  'pętla decyzyjna ',
+  'drabina eskalacyjna ',
+  'przesmyk suwalski ',
+  'wektor ',
+  'król Salomon ',
+  'deep penetration ',
+  'connectivity ',
+  'Heartland ',
+  'Anglosasi ',
+  'brama smoleńska ',
+  'przepływy strategiczne ',
+  'doktryna Giedroycia ',
+  'wykrok ',
+  'pauza geopolityczna ',
+  'koncert mocarstw ',
+  'matryca ',
+  'wieża ',
+  'strefa zgniotu ',
+  'Państwo Środka ',
+  'wschodnia flanka NATO ',
+  'equilibirum ',
+  'wysunięta kasztelania ',
+  'masy lądowe Eurazji ',
+  'kolaps ',
+  'Rimland ',
+  'Szlak Jedwabny ',
+  'junior partner ',
+  'Clausewitz ',
+  'punkt ciężkości ',
+  'gra statusowa ',
+  'sworzeń ',
+  'Lewant ',
+  'systemy anty- dostępowe ',
+  'ekspirowanie ',
+  'wojna hybrydowa ',
+  'stronnictwo kontynentalne ',
+  'obszar rdzeniowy ',
+  'efektor ',
+  'długie trwanie ',
+  'iunctim ',
+  'domena kosmiczna ',
+  'agregacja informacji ',
+  'Boca Chica ',
+  'baronowie kosmosu ',
+  'zeloci niepodległości ',
+  'neoantlantyści ',
+  'mapy mentalne ',
+  'dualizm na Łabie ',
+  'układ sił ',
+  'pułapka Tukidydesa ',
+  'game-changer'
 ];
 
 let templateArray = [
@@ -99,7 +100,7 @@ function put_in() {
 
 function back(){
   for(let i=0; i < ArrayB.length; i++) {
-  Array.push(ArrayB[i]);
+    Array.push(ArrayB[i]);
   }
   ArrayB=[];
 }
@@ -107,18 +108,28 @@ function back(){
 function klik() {
   for (let i = 0; i < tile.length; i++) {
     tile[i].addEventListener('click', () => {
-      r = getRandom(0, templateArray.length);
-      let pick = tile[i].children[0].textContent;
-      pickArray.push(pick);
-      notespan.textContent = templateArray[r]+pickArray;
+      if(pickArray.length<5){
+        r = getRandom(0, templateArray.length);
+        let pick = tile[i].children[0].textContent;
+        pickArray.push(pick);
+        notespan.textContent = templateArray[r]+pickArray+" ";
+      }else{
+        notespan.textContent = "Wybrałeś już 5 wartości; teraz naciśnij CONNECTIVITY by otrzymać wypowiedź dr Bartosiaka.";
+      }
     });
   }
 }
 
 function generate() {
   button.addEventListener('click', () => {
-    console.log('kliknąłeś na przycisk');
-    return (notespan.textContent = `Najpilniejsze wyzwanie, jakiemu musi sprostać Rzeczypospolita w obecnym klimacie geopolitycznym to redefinicja pojęcia ${pickArray[0]}. W swoich analizach dla "Strategy&Future" niejednokrotnie wykazywałem znacznie, jakie w architekturze mentalnej tego terminu mają zarówno ${pickArray[1]}, jak i ${pickArray[2]}. Tymczasem aparat responsywny państwa polskiego skupiał się dotąd na reliktach zmurszałych koncepcji takich jak ${pickArray[3]}, prowadząc nieuchronnie do utraty sprawczości w regionie i zdania się na łaskę tego, co w polskiej martyrologii dziejowej zwykliśmy definiować kryptonimem ${pickArray[4]}.`);
+    let speech = [
+    `Najpilniejsze wyzwanie, jakiemu musi sprostać Rzeczypospolita w obecnym klimacie geopolitycznym to redefinicja pojęcia ${pickArray[0]}. W swoich analizach dla "Strategy&Future" niejednokrotnie wykazywałem znacznie, jakie w architekturze symbolicznej tego terminu mają zarówno ${pickArray[1]}, jak i ${pickArray[2]}. Tymczasem aparat responsywny państwa polskiego skupiał się dotąd na reliktach zmurszałych koncepcji takich jak ${pickArray[3]}, prowadząc nieuchronnie do utraty sprawczości w regionie i zdania się na łaskę tego, co w polskiej martyrologii dziejowej zwykliśmy definiować kryptonimem ${pickArray[4]}.`,
+    `Wystarczy spojrzeć na mapę, aby pojąć dwa czynniki decydujące w walce o świątową hegemonię; są nimi dzisiaj ${pickArray[0]} oraz ${pickArray[1]}. Zdynamizowane tarcia wśród powyższych, nieuchronnie zaprowadzi Rzeczypospolitą do konieczności powiedzenia "sprawdzam" wobec naszych sojuszników, co z kolei popchnie zapewne elity Zachodu do kaskadowej erozji elementów ładu światowego, takich jak ${pickArray[2]}. Będziemy zatem świadkami eskalacji nowych zagrożeń, o których dobitnie świadczą ${pickArray[3]}, a w jeszcze większym stopniu ${pickArray[4]}`,
+    `Żyjemy w ciekawych czasach, choć cokolwiek niebezpiecznych. Z jednej strony ${pickArray[0]} to poważne wyzwanie dla Rzeczypospolitej, które prowadzi do sytuacji bezalternatywnej: albo ${pickArray[1]}, albo ${pickArray[2]}. W tej sytuacji lepiej rozumiemy dylematy Becka i polskiej polityki zagranicznej w 39r., która zogniskowała się wokół determinant takich jak ${pickArray[3]}. Co z tego wynika dla naszej teraźniejszości? Ujmując rzecz najbardziej dobitnie i w pełnym wymiarze ostrości, odpowiem krótko: ${pickArray[4]}.`,
+    `Jaką drogą powinna podążyć Rzeczypospolita Anno Domini 2021? Wobec zdynamizowania tarć geostrategicznych w regionie, dotychczasowi gwaranci naszego bezpieczeństwa, za jakich uchodzili ${pickArray[0]} oraz ${pickArray[1]}, zdają się tracić obecnie na znaczeniu. W tej sytuacji ${pickArray[2]} to zupełnie nowe zjawisko dla świata Zachodu, a jedyną skuteczną reakcją na utratę wpływów może być sięgniecie do rezerwuaru praktyk semi-autorytarnych, które najlepiej uosabiają ${pickArray[3]}, a w radykalnym wymiarze również ${pickArray[4]}.`
+  ];
+    r=getRandom(0,speech.length)
+    return (notespan.textContent = speech[r]);
     }
   )
 }
