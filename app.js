0dc80db6f3832f2696ff54ff7eb49da7ed81e494
Radek Nowakowski <rad.nowakowski@gmail.com>
2021-10-05 23:24:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ let Array = [
 ];
 
 let templateArray = [
-  `Doktor Jacek Bartosiak wyjaśni `
+  `Doktor Jacek Bartosiak wyjaśni: `
 ];
 
 let ArrayB = [];
@@ -114,7 +114,7 @@ function klik() {
         pickArray.push(pick);
         notespan.textContent = templateArray[r]+pickArray+" ";
       }else{
-        notespan.textContent = "Wybrałeś już 5 wartości; teraz naciśnij CONNECTIVITY by otrzymać wypowiedź dr Bartosiaka.";
+        notespan.textContent = templateArray[r]+pickArray+". \n"+"Wybrałeś już 5 wartości; teraz naciśnij CONNECTIVITY by otrzymać wypowiedź dr Bartosiaka.\n";
       }
     });
   }
@@ -124,9 +124,9 @@ function generate() {
   button.addEventListener('click', () => {
     let speech = [
     `Najpilniejsze wyzwanie, jakiemu musi sprostać Rzeczypospolita w obecnym klimacie geopolitycznym to redefinicja pojęcia ${pickArray[0]}. W swoich analizach dla "Strategy&Future" niejednokrotnie wykazywałem znacznie, jakie w architekturze symbolicznej tego terminu mają zarówno ${pickArray[1]}, jak i ${pickArray[2]}. Tymczasem aparat responsywny państwa polskiego skupiał się dotąd na reliktach zmurszałych koncepcji takich jak ${pickArray[3]}, prowadząc nieuchronnie do utraty sprawczości w regionie i zdania się na łaskę tego, co w polskiej martyrologii dziejowej zwykliśmy definiować kryptonimem ${pickArray[4]}.`,
-    `Wystarczy spojrzeć na mapę, aby pojąć dwa czynniki decydujące w walce o świątową hegemonię; są nimi dzisiaj ${pickArray[0]} oraz ${pickArray[1]}. Zdynamizowane tarcia wśród powyższych, nieuchronnie zaprowadzi Rzeczypospolitą do konieczności powiedzenia "sprawdzam" wobec naszych sojuszników, co z kolei popchnie zapewne elity Zachodu do kaskadowej erozji elementów ładu światowego, takich jak ${pickArray[2]}. Będziemy zatem świadkami eskalacji nowych zagrożeń, o których dobitnie świadczą ${pickArray[3]}, a w jeszcze większym stopniu ${pickArray[4]}`,
+    `Wystarczy spojrzeć na mapę, aby pojąć dwa czynniki decydujące w walce o świątową hegemonię; są nimi dzisiaj ${pickArray[0]} oraz ${pickArray[1]}. Zdynamizowanie tarć wśród powyższych, nieuchronnie zaprowadzi Rzeczypospolitą do konieczności powiedzenia "sprawdzam" wobec naszych sojuszników, co z kolei popchnie zapewne elity Zachodu do kaskadowej erozji elementów ładu światowego, takich jak ${pickArray[2]}. Będziemy zatem świadkami eskalacji nowych zagrożeń, o których dobitnie świadczą ${pickArray[3]}, a w jeszcze większym stopniu ${pickArray[4]}`,
     `Żyjemy w ciekawych czasach, choć cokolwiek niebezpiecznych. Z jednej strony ${pickArray[0]} to poważne wyzwanie dla Rzeczypospolitej, które prowadzi do sytuacji bezalternatywnej: albo ${pickArray[1]}, albo ${pickArray[2]}. W tej sytuacji lepiej rozumiemy dylematy Becka i polskiej polityki zagranicznej w 39r., która zogniskowała się wokół determinant takich jak ${pickArray[3]}. Co z tego wynika dla naszej teraźniejszości? Ujmując rzecz najbardziej dobitnie i w pełnym wymiarze ostrości, odpowiem krótko: ${pickArray[4]}.`,
-    `Jaką drogą powinna podążyć Rzeczypospolita Anno Domini 2021? Wobec zdynamizowania tarć geostrategicznych w regionie, dotychczasowi gwaranci naszego bezpieczeństwa, za jakich uchodzili ${pickArray[0]} oraz ${pickArray[1]}, zdają się tracić obecnie na znaczeniu. W tej sytuacji ${pickArray[2]} to zupełnie nowe zjawisko dla świata Zachodu, a jedyną skuteczną reakcją na utratę wpływów może być sięgniecie do rezerwuaru praktyk semi-autorytarnych, które najlepiej uosabiają ${pickArray[3]}, a w radykalnym wymiarze również ${pickArray[4]}.`
+    `Jaką drogą powinna podążyć Rzeczypospolita Anno Domini 2021? Wobec zdynamizowania tarć geostrategicznych w regionie, dotychczasowi gwaranci naszego bezpieczeństwa, a więc ${pickArray[0]} oraz ${pickArray[1]}, zdają się definitywnie tracić na znaczeniu. W tej sytuacji ${pickArray[2]} to zupełnie nowe zjawisko dla świata Zachodu, a jedyną skuteczną reakcją na utratę wpływów może być sięgniecie do rezerwuaru praktyk semi-autorytarnych, które najlepiej uosabiają ${pickArray[3]}, a w wariancie nuklearnym również ${pickArray[4]}.`
   ];
     r=getRandom(0,speech.length)
     return (notespan.textContent = speech[r]);
